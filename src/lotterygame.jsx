382a948336f36c0a28395e974a977cbadb716b99
Rahul-Ranjan-dev <rahulranjan9610@gmail.com>
2026-06-29 23:55:20 +0530

    small changes

--- src/lotterygame.jsx
+++ src/lotterygame.jsx
@@ -1,30 +1,31 @@
 import { useState } from "react"
 export default function Lottery(){
   const[number,setnumber]=useState("")
-  const[attempted,setattempted]=useState([])
+  const[sum,setsum]=useState(null)
   function handleChange(event){
     setnumber(event.target.value)
   }
   function handlesubmit(){
 
-    let sum=0;
+    let total=0;
     let remainder=0;
     let tempnum=parseInt(number)
     while(tempnum>0){
         remainder=tempnum%10
         tempnum=Math.floor(tempnum/10);
-        sum=sum+remainder;
+        total=total+remainder;
 
     }
    
-    if(sum===15){
-       alert("you won the game")
+    // if(sum===15){
+    //    alert("you won the game")
      
-    }
-        else{
-          alert("try again")
+    // }
+    //     else{
+    //       alert("try again")
         
-        }
+    //     }
+        setsum(total)
         setnumber("")
         
     }
@@ -33,6 +34,8 @@ export default function Lottery(){
     <>
     enter your lotterynumber:<input type="number" max="3" value={number} onChange={handleChange}/>
     <button onClick={handlesubmit}>submit</button>
+   
+    { sum>=15? (<p>you won the game</p>):(<p>try again</p>)}
     </>
   )
 }
