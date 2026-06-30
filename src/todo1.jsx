f2a31947aaf5b819727561bea3929df70d4ba3b8
Rahul-Ranjan-dev <rahulranjan9610@gmail.com>
2026-07-01 00:05:51 +0530

    save changes

--- src/todo1.jsx
+++ src/todo1.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import { v4 as uuid } from "uuid";
 export default function Todo1(){
-    const [tasks,settasks]=useState([{v:"sample task",id:uuid()}]); 
+    const [tasks,settasks]=useState([{v:"sample task",id:uuid(),isMarked:false}]); 
     const [newTask,setnewTask]=useState("");
     const [toggle,settoggle]=useState(true)
     function handleChange(event){
        setnewTask(event.target.value)
     }
     function handleSubmit(){
-        settasks([...tasks,{v:newTask,id:uuid()}])
+        settasks([...tasks,{v:newTask,id:uuid(),isMarked:false}])
         setnewTask("")
     }
     function handleUpdate(id){
@@ -23,14 +23,32 @@ export default function Todo1(){
     })
       settasks(updateTask)
     }
+    function handleDelete(id){
+      const updateTask=tasks.filter((task)=>task.id!==id)
+      settasks(updateTask)
+    }
+    function handleMark(id){
+          const MarkTask=tasks.map((task)=>{
+            if(task.id==id){
+              return   {...task,isMarked:!task.isMarked}
+            }
+            else{
+                return task;
+            }
+            
+          })
+          settasks(MarkTask)
+    }
     return(
         <>
         
         <label htmlFor="newtask">enter new task</label>
         <input id="newtask" placeholder="enter new task" value={newTask} onChange={handleChange} />
         <button onClick={handleSubmit}>add new</button>
-        <ul>{tasks.map((task)=><li key={task.id}>{task.v}
+        <ul>{tasks.map((task)=><li key={task.id} style={{ textDecoration:task.isMarked==true? "line-through":"none"}}>{task.v}
             <button onClick={()=>handleUpdate(task.id)}>update</button>
+            <button onClick={()=>handleDelete(task.id)}>delete</button>
+            <button onClick={()=>handleMark(task.id)}>markasdone</button>
         </li>)}</ul>
         </>
     )
